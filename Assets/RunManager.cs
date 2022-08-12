d135bcb61fbe75c7475cf1676d3f813f5a9d8b68
RowanMoss <68680289+RowanMoss@users.noreply.github.com>
2022-08-12 12:16:32 +1200

    Run scenes

---
+++ Assets/RunManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunManager : MonoBehaviour
+{
+
+    GameObject player;
+    bool lastLevel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        DontDestroyOnLoad(player);
+        DontDestroyOnLoad(gameObject);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartNewLevel()
+    {
+        player.transform.position = new Vector3(60, 5, 60);
+    }
+}
