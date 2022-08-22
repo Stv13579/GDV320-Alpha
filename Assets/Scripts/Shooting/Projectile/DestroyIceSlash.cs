29d34e740a43a71cd307e78e213d53c32dd09175
aydenlimyip <65572042+aydenlimyip@users.noreply.github.com>
2022-08-22 15:58:36 +1200

    ice slash hit box

---
+++ Assets/Scripts/Shooting/Projectile/DestroyIceSlash.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyIceSlash : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask environment;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(((1<<other.gameObject.layer) & environment) != 0)
+        {
+            Destroy(gameObject.transform.parent.gameObject);
+        }
+    }
+}
