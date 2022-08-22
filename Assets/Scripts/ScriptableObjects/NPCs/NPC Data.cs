1498dcc4765c09f698132744dd137e00f65013a7
RowanMoss <68680289+RowanMoss@users.noreply.github.com>
2022-08-22 12:43:27 +1200

    NPC dialogue and systems to support them

--- Assets/Scripts/ScriptableObjects/NPCs/NPC Data.cs
+++ Assets/Scripts/ScriptableObjects/NPCs/NPC Data.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 [CreateAssetMenu]
 public class NPCData : ScriptableObject
 {
-
+    //Where the player is in the overall story of the NPC
     public int storyPosition;
+    //How much of the current story the player has seen
+    public int intraStoryPosition;
 
     public bool questReady = false;
+    public bool onQuest = false;
+
+    public List<NPC.Dialogue> seenStoryPoints = new List<NPC.Dialogue>(); 
 
 
 }
