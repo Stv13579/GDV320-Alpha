e5f156f35c6c17516487635e471872aa758b4e50
Stv13579 <48304098+Stv13579@users.noreply.github.com>
2022-09-05 13:22:19 +1200

    Finished flying anim setup, shield enemy anims implemented

---
+++ Assets/RangedAnimationHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedAnimationHandler : MonoBehaviour
+{
+	public void Attack()
+	{
+		this.transform.parent.gameObject.GetComponent<RangedEnemyScript>().Attack();
+	}
+	
+}
