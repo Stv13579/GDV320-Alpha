e56527050007e5851d2555b23a453f13a0998977
RowanMoss <68680289+RowanMoss@users.noreply.github.com>
2022-09-30 15:29:24 +1300

    Shield enemy moves goodly

---
+++ Assets/CapHitter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapHitter : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            GetComponentInParent<ShieldEnemyScript>().CapDamage();
+            GetComponentInParent<ShieldEnemyScript>().capHitter.SetActive(false);
+        }
+    }
+}
