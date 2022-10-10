02f85395d5c6ca806de43b9f53a32e3411fef232
RowanMoss <68680289+RowanMoss@users.noreply.github.com>
2022-10-10 14:15:10 +1300

    Create CrystalElement.cs

---
+++ Assets/Scripts/Shooting/CrystalElement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalElement : BaseElementClass
+{
+    [SerializeField]
+    GameObject crystalProjectile;
+
+    [SerializeField]
+    float projectileSpeed;
+
+    [SerializeField]
+    AnimationCurve damageCurve;
+
+    [SerializeField]
+    float lifeTimer;
+
+    [SerializeField]
+    float damageLimit;
+
+    [SerializeField]
+    float damageSpeedDecreaser;
+
+    public override void ElementEffect()
+    {
+        base.ElementEffect();
+        // for loop to instaniate it 5 times
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+
+                GameObject newCrystalPro = Instantiate(crystalProjectile, shootingTranform.position, Camera.main.transform.rotation);
+                //changes the angle of where they are being fired to
+                newCrystalPro.transform.RotateAround(shootingTranform.position, Camera.main.transform.up, Random.Range(1.0f, 3.0f) * i - Random.Range(1.0f, 5.0f));
+                newCrystalPro.transform.RotateAround(shootingTranform.position, Camera.main.transform.right, Random.Range(1.0f, 3.0f) * j - Random.Range(1.0f, 5.0f));
+                // setting the varibles from CrystalProj script
+	            newCrystalPro.GetComponent<CrystalProj>().SetVars(projectileSpeed, damage * (damageMultiplier * elementData.crystalDamageMultiplier), damageCurve, lifeTimer, attackTypes, damageLimit, damageSpeedDecreaser);
+            }
+        }
+        //RaycastHit hit;
+        //Physics.Raycast(this.gameObject.transform.position, Camera.main.transform.forward, out hit, 100, shootingIgnore);
+        //if (hit.collider)
+        //{
+        //    crystalProjectile.transform.LookAt(hit.point);
+        //}
+    }
+
+    public override void ActivateVFX()
+    {
+        base.ActivateVFX();
+    }
+
+    protected override void StartAnims(string animationName, string animationNameAlt = null)
+    {
+        randomAnimationToPlay = Random.Range(0, 2);
+        if (randomAnimationToPlay == 0)
+        {
+            playerHand.SetTrigger(animationName);
+            if (audioManager)
+            {
+                audioManager.StopSFX(shootingSoundFX);
+                audioManager.PlaySFX(shootingSoundFX);
+            }
+        }
+        else
+        {
+            playerHand.SetTrigger(animationNameAlt);
+            if (audioManager)
+            {
+                audioManager.StopSFX(otherShootingSoundFX);
+                audioManager.PlaySFX(otherShootingSoundFX);
+            }
+        }
+    }
+}
