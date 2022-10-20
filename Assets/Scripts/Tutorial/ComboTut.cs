7640753015e7a18738f7c360880f5f684e7d959c
RowanMoss <68680289+RowanMoss@users.noreply.github.com>
2022-10-20 18:01:02 +1300

    Freya anims in

---
+++ Assets/Scripts/Tutorial/ComboTut.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComboTut : TutorialPointers
+{
+    public override void CheckConditions(bool done = false)
+    {
+        bool result = false;
+
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            result = true;
+        }
+
+
+        base.CheckConditions(result);
+    }
+}
---
+++ Assets/Scripts/Tutorial/WalkTut.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkTut : TutorialPointers
+{
+    public override void CheckConditions(bool done = false)
+    {
+        bool result = false;
+
+        if (Input.GetKeyUp(KeyCode.W) ||
+            Input.GetKeyUp(KeyCode.A) ||
+            Input.GetKeyUp(KeyCode.S) ||
+            Input.GetKeyUp(KeyCode.D))
+        {
+            result = true;
+        }
+        base.CheckConditions(result);
+    }
+}
