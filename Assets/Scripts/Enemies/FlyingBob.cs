86bd480726f26db1a1d82f48279783a8a2afca4d
Rowan Moss <Rowan.Moss@mds.ac.nz>
2022-11-01 16:53:08 +1300

    Journal In

---
+++ Assets/Scripts/Enemies/FlyingBob.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingBob : MonoBehaviour
+{
+    [SerializeField]
+    BaseFlyingEnemyScript flyingScript;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        flyingScript.HitFloor();
+    }
+}
