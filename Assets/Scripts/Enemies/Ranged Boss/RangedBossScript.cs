66fe798a9e089c74c7eaf94a5f633b68c69a508a
Stv13579 <48304098+Stv13579@users.noreply.github.com>
2022-11-02 14:16:55 +1300

    Meta files

--- Assets/Scripts/Enemies/Ranged Boss/RangedBossScript.cs
+++ Assets/Scripts/Enemies/Ranged Boss/RangedBossScript.cs
@@ -28,7 +28,7 @@ public class RangedBossScript : BaseEnemyClass //Sebastian
 	bool waterAttacking = false;
 	int homingAttack = 5;
 	int homingAttackCounter = 0;
-	float hurtTimer = 0.2f;
+	float invinceTimer = 0.2f;
 
     public override void Awake()
     {
@@ -53,7 +53,7 @@ public class RangedBossScript : BaseEnemyClass //Sebastian
     public override void Update()
 	{
 		base.Update();
-		hurtTimer -= Time.deltaTime;
+		invinceTimer -= Time.deltaTime;
 		this.GetComponent<MeshCollider>().sharedMesh = this.transform.GetChild(2).GetChild(0).GetComponent<SkinnedMeshRenderer>().sharedMesh;
         if(enemyAnims.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
         {
@@ -173,10 +173,10 @@ public class RangedBossScript : BaseEnemyClass //Sebastian
 	
 	public override void TakeDamage(float damageToTake, List<Types> attackTypes, float extraSpawnScale = 1, bool applyTriggers = true)
 	{
-		if(hurtTimer <= 0)
+		if(invinceTimer <= 0)
 		{
 			base.TakeDamage(damageToTake, attackTypes, extraSpawnScale, applyTriggers);
-			hurtTimer = 0.1f;
+			invinceTimer = 0.1f;
 		}
 	}
 	
